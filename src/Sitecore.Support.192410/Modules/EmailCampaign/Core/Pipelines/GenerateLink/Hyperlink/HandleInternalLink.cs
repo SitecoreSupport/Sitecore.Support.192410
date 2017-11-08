0d3bca9bfc63e76a93a7179a108fc11ef4f629cb
AntonKechashin <atk@sitecore.net>
2017-11-08 13:01:38 +0200

    Initial commit

---
+++ src/Sitecore.Support.192410/Modules/EmailCampaign/Core/Pipelines/GenerateLink/Hyperlink/HandleInternalLink.cs
@@ -0,0 +1,21 @@
+﻿namespace Sitecore.Support.Modules.EmailCampaign.Core.Pipelines.GenerateLink.Hyperlink
+{
+  using Sitecore.Modules.EmailCampaign.Core;
+  using Sitecore.Modules.EmailCampaign.Core.Pipelines.GenerateLink;
+  using Sitecore.Sites;
+
+  public class HandleInternalLink : Sitecore.Modules.EmailCampaign.Core.Pipelines.GenerateLink.Hyperlink.HandleInternalLink
+  {
+    public override void Process(GenerateLinkPipelineArgs args)
+    {
+      base.Process(args);
+      #region Added code
+      if (args.MailMessage.InnerItem.TemplateID.ToString() == TemplateIds.RichTextMessage)
+      {
+        string remove = "://" + SiteContext.GetSite(args.WebsiteConfigurationName).HostName + "/";
+        args.Url = args.Url.Replace(remove, "");
+      }
+      #endregion
+    }
+  }
+}
